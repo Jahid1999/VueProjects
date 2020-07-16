bf523d0524c1bda4f551499d2a8d0a0898d9dd6d
Jahid1999 <bsse1030@iit.du.ac.bd>
2020-07-16 16:19:40 +0600

    quasar first

---
+++ quasar-1/src/App.vue
@@ -0,0 +1,91 @@
+<template>
+  <q-layout view="lHh Lpr fFf">
+      <q-header elevated >
+       <q-toolbar  class="bg-green text-white  shadow-2 ">
+      <q-btn flat round dense icon="arrow_back"/>
+      <q-space/>
+      <q-toolbar-title>
+        Team 11
+      </q-toolbar-title>
+      
+      <q-btn flat round dense icon="money" class="q-mr-xs" />
+      <q-btn flat round dense icon="info" />
+    </q-toolbar>
+    <q-toolbar class="bg-secondary">
+      <q-avatar>
+          <img src="./assets/logo.png">
+      </q-avatar>
+        <div class="teamName"> Quasar </div>
+
+        <q-space/>
+
+          <q-avatar size="24px" color="white">
+            <img src="./assets/vs.png">
+            </q-avatar>
+          <q-space/>
+
+        <div class="teamName"> Jahid </div>
+      <q-avatar>
+          <img src="./assets/jahid.jpg">
+      </q-avatar>
+        
+
+    </q-toolbar>
+    <q-toolbar class="bg-warning">
+      <q-space/>
+      <q-toolbar-title>
+        Remaining times
+      </q-toolbar-title>
+      <q-space/>
+    </q-toolbar>
+      </q-header>
+
+    <q-page-container >
+      <q-page padding style="padding-top: 66px">
+          <HelloWorld />
+      </q-page>
+      
+     
+    </q-page-container>
+    
+    <q-footer class="bg-grey-5">
+      <div class="q-mb-md text-center q-pt-sm q-gutter-sm">
+        <q-btn color=green text-color="black" glossy  label="Create Own Team" /> 
+        
+      </div>
+    </q-footer>
+  </q-layout>
+
+
+
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+export default {
+  name: 'LayoutDefault',
+
+  components: {
+    HelloWorld
+  },
+
+  data () {
+    return {
+      leftDrawerOpen: false
+    }
+  }
+}
+</script>
+
+<style>
+  .hudai{
+    text-align: center;
+    
+  }
+  .teamName {
+    color: black;
+    font-size: 20px;
+    padding: 10px;
+    font-weight: 500;
+  }
+</style>
---
+++ vuetify_2/src/App.vue
@@ -0,0 +1,77 @@
+<template>
+<div>
+  <v-app>
+    
+    <template>
+     
+        <v-navigation-drawer
+          permanent
+          expand-on-hover
+          color="#80CBC4"
+          height=40%
+        >
+          <v-list>
+            <v-list-item class="px-2">
+              <v-list-item-avatar>
+                <v-img src="./assets/jahid.jpg"></v-img>
+              </v-list-item-avatar>
+            </v-list-item>
+
+            <v-list-item link>
+              <v-list-item-content>
+                <v-list-item-title class="title">Abdullah Al Jahid</v-list-item-title>
+                <v-list-item-subtitle>bsse1030@iit.du.ac.bd</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
+
+          <v-divider></v-divider>
+
+          <v-list
+            nav
+            dense
+          >
+            <v-list-item link>
+              <v-list-item-icon>
+                <v-icon>mdi-folder</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>My Files</v-list-item-title>
+            </v-list-item>
+            <v-list-item link>
+              <v-list-item-icon>
+                <v-icon>mdi-account-multiple</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Shared with me</v-list-item-title>
+            </v-list-item>
+            <v-list-item link>
+              <v-list-item-icon>
+                <v-icon>mdi-star</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Starred</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-navigation-drawer>
+     
+    </template>
+    
+    <v-main> <router-link></router-link> </v-main>
+</v-app>
+
+  </div>  
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+
+  components: {
+   
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
